d756b061151f5442a515a2e735b3f5da0138983c
golamkibria11265@gmail.com <you@example.com>
2021-12-17 19:07:34 +0600

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-def prime_number_checker(number):
-    prime_number = True
-    for i in range(2, number):
-        if number % i == 0:
-            prime_number = False
-    if prime_number:
-        print("This is a prime number")
-    else:
-        print("This is not a prime number")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 
-numbers = int(input("Please enter a number:"))
-prime_number_checker(numbers)
+def encode(txt, k):
+    shifted_list = ""
+    for i in txt:
+        if i in alphabet:
+            position = alphabet.index(i)
+            shifted_list += alphabet[position + k]
+    print("Encoded Message is", shifted_list)
+
+
+def decode(txt, k):
+    shifted_list = ""
+    for i in txt:
+        if i in alphabet:
+            position = alphabet.index(i)
+            shifted_list += alphabet[position - k]
+    print("Decoded Message is", shifted_list)
+
+
+condition_check = True
+
+while condition_check:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction == "encode":
+        print("Mission is success")
+        text = input("Type your message to encode:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        encode(text, shift)
+    if direction == "decode":
+        print("Mission is success")
+        text = input("Type your message to decode:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        decode(text, shift)
